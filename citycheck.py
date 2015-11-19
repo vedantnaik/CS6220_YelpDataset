54ccc731bc5a1db9632a81ee878493ac490184e9
Dixit <dixitk13@ccs.neu.edu>
2015-11-19 01:13:58 -0500

    adding generic functions and mappings

--- citycheck.py
+++ citycheck.py
@@ -1,1 +1,35 @@
 __author__ = 'Dixit_Patel'
+import json
+
+
+line = '{"business_id": "2EKGrbf2_81MrtjKZeOTng", "full_address": "Kaiserstr. 93\n76133 Karlsruhe", "hours": {}, "open": false, "categories": ["Food", "Coffee & Tea"], "city": "Karlsruhe", "review_count": 5, "name": "coffee fellows", "neighborhoods": [], "longitude": 8.4091588999999995, "state": "BW", "stars": 2.5, "latitude": 49.009267600000001, "attributes": {}, "type": "business"}'
+line = line.replace("\n","")
+lineJson = json.loads(line)
+businessId = lineJson.get("business_id")
+type = lineJson.get("type")
+
+print "=====>", businessId, type
+
+
+businessIdList = []
+with open(r"C:\\yelp_dataset_challenge_academic_dataset\yelp_academic_dataset_business.json") as businessFile:
+    for line in businessFile.read().split("\n") if len(line) > 0:
+        line = line.__str__()
+        line = line.replace("\\n","").replace("\\t","").replace("\\r","")
+        print line, "-"
+        lineJson = json.loads(line)
+        businessId = lineJson.get("business_id")
+        type = lineJson.get("type")
+        if lineJson.get("city") == "New York":
+            businessIdList.append(lineJson.get("business_id"))
+print "businesslist", len(businessIdList)
+reviews = []
+with open(r"C:\\yelp_dataset_challenge_academic_dataset\yelp_academic_dataset_review.json") as businessFile:
+    for line in businessFile.read().split("\n"):
+        line = line.__str__()
+        lineJson = json.loads(line)
+        if businessIdList.__contains__(lineJson.get("")):
+            reviews.append(lineJson)
+
+for review in reviews:
+    print review
--- test.py
+++ test.py
@@ -1,1 +1,21 @@
 __author__ = 'Dixit_Patel'
+import json
+def test():
+    def recursive(hash_thing):
+        if isinstance(hash_thing, dict):
+            hash_thing = {k: v for k, v in hash_thing.items() if v is not None}
+            for k,v in hash_thing.items():
+                if isinstance(v, dict):
+                    hash_thing[k] = recursive(hash_thing[k])
+                    print " sa ", recursive(hash_thing[k])
+            return hash_thing
+        else:
+            return hash_thing
+
+    line = '{"business_id": "7VaXyrIw57IFNXzhYFn16g", "full_address": "8320 W Hayden Rd Scottsdale, AZ 85258", "hours": {"Monday": {"close": "18:00", "open": "06:30"}, "Tuesday": {"close": "18:00", "open": "06:30"}, "Friday": {"close": "18:00", "open": "06:30"}, "Wednesday": {"close": "18:00", "open": "06:30"}, "Thursday": {"close": "18:00", "open": "06:30"}, "Sunday": {"close": "18:00", "open": "07:00"}, "Saturday": {"close": "18:00", "open": "07:00"}}, "open": false, "categories": ["Food", "Juice Bars & Smoothies", "Pets", "Pet Stores"], "city": "Scottsdale", "review_count": 3, "name": "In The Raw", "neighborhoods": [], "longitude": -111.89871309999999, "state": "AZ", "stars": 2.5, "latitude": 33.557718100000002, "attributes": {"Parking": {"garage": false, "street": false, "validated": false, "lot": false, "valet": false}, "Accepts Credit Cards": {}, "Price Range": 2}, "type": "business"}'
+    line = recursive(json.loads(line))
+
+    print line, "-"
+
+if __name__ == '__main__':
+    test()
