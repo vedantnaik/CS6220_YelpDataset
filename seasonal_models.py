a42c37a469b4c556a4ac7b535fb07a0283c0438b
Dixit Patel <dixitk13@gmail.com>
2015-12-04 14:36:35 -0500

    Adding acf and pacf functions

--- seasonal_models.py
+++ seasonal_models.py
@@ -1,9 +1,9 @@
-__author__ = 'Dixit_Patel'
-
 import pandas as pd
 import matplotlib.pyplot as plt
 import statsmodels.api as sm
+import numpy as np
 
+__author__ = 'Dixit_Patel'
 
 '''
 this fun does seasonal decomposition
@@ -17,7 +17,7 @@ def seasonal_decomposition(file_name):
     df['review_count_lambda_smooth'] = df['review_count'].apply(lambda x: x+1)
 
     df.index = pd.DatetimeIndex(df['Date'])
-    df = df.resample('M',how='mean')
+    df = df.resample('M', how='mean')
     df = df.fillna(1)
 
     # fig,ax = plt.subplots(1,1, figsize=(6,4))
@@ -29,5 +29,72 @@ def seasonal_decomposition(file_name):
     plt.legend()
     plt.show()
 
+'''
+applies acf, pacf and ARIMA prediction on the timeseries
+given as csv format
+'''
+
+
+def auto_regression_moving_averages(file_name):
+
+    df = ['Index', 'Date', 'review_count']
+    df = pd.read_csv(file_name, delimiter=',').dropna()
+    df['review_count_lambda_smooth'] = df['review_count'].apply(lambda x: x+1)
+
+    df.index = pd.DatetimeIndex(df['Date'])
+    df = df.resample('M', how='mean')
+    df = df.fillna(1)
+
+    flow = df['review_count_lambda_smooth']
+
+    # clean data fram
+    df = df.drop(['review_count'], axis=1)
+
+    fig = plt.figure(figsize=(12, 8))
+    ax = fig.add_subplot(211)
+    fig = sm.graphics.tsa.plot_pacf(flow, lags=40, ax=ax)
+
+    ax = fig.add_subplot(212)
+    fig = sm.graphics.tsa.plot_acf(flow, lags=40, ax=ax)
+
+    arma_mod20 = sm.tsa.ARMA(flow, (2, 0)).fit()
+    print('=')
+    print(arma_mod20.params)
+    print('=')
+    arma_mod30 = sm.tsa.ARMA(flow, (3, 0)).fit()
+
+    print('==')
+    print(arma_mod20.aic, arma_mod20.bic, arma_mod20.hqic)
+    print('==')
+
+    print('===')
+    print(arma_mod30.params)
+    print('===')
+
+    resid = arma_mod30.resid
+    sm.graphics.qqplot(resid, line='q', fit=True)
+
+    fig = plt.figure(figsize=(12, 8))
+    ax1 = fig.add_subplot(411)
+    fig = sm.graphics.tsa.plot_acf(resid.values.squeeze(), lags=40, ax=ax1)
+    ax2 = fig.add_subplot(412)
+    fig = sm.graphics.tsa.plot_pacf(resid, lags=40, ax=ax2)
+
+    r, q, p = sm.tsa.acf(resid.values.squeeze(), qstat=True)
+    data = np.c_[range(1, 41), r[1:], q, p]
+    table = pd.DataFrame(data, columns=['lag', "AC", "Q", "Prob(>Q)"])
+    print(table.set_index('lag'))
+
+    predict_reviews = arma_mod30.predict('2012', '2015', dynamic=True)
+    print(predict_reviews)
+
+    fig, ax = plt.subplots(figsize=(12, 8))
+    ax = df.ix['2005':].plot(ax=ax)
+    fig = arma_mod30.plot_predict('2012', '2015', dynamic=True, ax=ax, plot_insample=False)
+
+    plt.legend()
+    plt.show()
+
 if __name__ == '__main__':
-    seasonal_decomposition('resources/year_all_review_count.csv')
+    # seasonal_decomposition('resources/year_all_review_count.csv')
+    auto_regression_moving_averages('resources/year_all_review_count.csv')
